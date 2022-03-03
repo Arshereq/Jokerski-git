d7936d755777c028a5be39398ad82f96401af72d
Sebastian <oporowymakus21@gmail.com>
2022-03-03 09:56:55 +0100

    [BUG]fix depends on database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
