94ba4c9b18226871f964da594dd0ee563d23ef10
Sebastian <oporowymakus21@gmail.com>
2022-03-10 12:17:29 +0100

    Fix bash script

--- backend/start.sh
+++ backend/start.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-python manage.py makemigrations
+apt-get update
+apt-get install libpq-dev --assume-yes
+
+# # Collect static files
+# echo "Collect static files"
+# python manage.py collectstatic --noinput
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py makemigrations --noinput
 python manage.py migrate
+
+echo Starting Gunicorn.
 gunicorn -b 0.0.0.0:8000 --worker-class=gevent --worker-connections=1000 --workers=5 backend.wsgi
