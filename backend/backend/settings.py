df3df79631a1ca1f20d6268c094778ba7c90e6ff
Sebastian <oporowymakus21@gmail.com>
2022-03-10 11:39:03 +0100

    Switch frontend port to 3000, add csrf trusted origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
