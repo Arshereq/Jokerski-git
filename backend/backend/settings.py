39f68f7118fc5dd999b118591646615ca64b3b4e
Paweł <biter0o1@o2.pl>
2022-03-13 16:20:48 +0100

    Add model to User

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
