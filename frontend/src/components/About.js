2fed2ce26bcea00e7e2b19991cf8424d9e2a77b0
dmurawski <dmurawski018@gmail.com>
2022-03-22 01:24:41 +0100

    add components and sample forms

---
+++ frontend/src/components/About.js
@@ -0,0 +1,18 @@
+function About() {
+  return (
+    <section class="hero">
+      <div class="hero-body">
+        <ol>
+          <li>Bartosz Niewiadomski</li>
+          <li>Damian Murawski</li>
+          <li>Daniel Nieznański</li>
+          <li>Sebastian Murawski</li>
+          <li>Paweł Zaręba</li>
+        </ol>
+      </div>
+    </section>
+  );
+}
+export default About;
+
+
