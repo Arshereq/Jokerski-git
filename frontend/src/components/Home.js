c3d99a26c60cc8d993bdf77dc91eaf767d3b3b5c
dmurawski <dmurawski018@gmail.com>
2022-03-22 23:14:10 +0100

    change view Home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,9 +1,8 @@
 function Home(){
     return(
         <div class="container">
-            <p>
-                STRONA Home
-            </p>
+            <h1 class="is-size-1 m-4 has-text-centered">Strona główna</h1>  
+            <h3 class="title is-3 has-text-centered pb-6">Aplikacja poświęcona na zajęcia zespołowe</h3>
         </div>
     );
 }
