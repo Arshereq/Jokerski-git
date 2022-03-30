34990882e23c37712045d41d59bd1330e4dbd317
Paweł <biter0o1@o2.pl>
2022-03-30 21:19:04 +0200

    add get user by username

--- backend/pastes/schema.py
+++ backend/pastes/schema.py
@@ -1,12 +1,14 @@
 import graphene
 from .models import Paste
+from users.models import User
 
 from graphene_django import DjangoObjectType
 
 class PasteType(DjangoObjectType):
     class Meta:
         model = Paste
-        fields = ('id','title','text', 'expire_after','visibility','author')
+        fields = ('id', 'title', 'text', 'expire_after', 'visibility', 'author')
+
 
 class Query(graphene.ObjectType):
     pastes = graphene.List(PasteType,id=graphene.Int())
@@ -16,6 +18,7 @@ class Query(graphene.ObjectType):
             return Paste.objects.filter(id=id)
         return Paste.objects.all()
 
+
 class CreatePaste(graphene.Mutation):
     paste = graphene.Field(PasteType)
 
@@ -23,11 +26,18 @@ class CreatePaste(graphene.Mutation):
         title = graphene.String(required=True)
         text = graphene.String(required=True)
         expire_after = graphene.String()
-        visibility = graphene.String(required=True)
+        visibility = graphene.Boolean()
         author = graphene.String(required=True)
-    def mutate(self,info,title,text,expire_after,visibility,author):
-        paste = Paste(title=title,text=text,expire_after=expire_after,
-                    visibility=visibility, author=author)
+
+    def mutate(self, info, title, text, expire_after, visibility, author):
+        paste = Paste(
+            title=title,
+            text=text,
+            expire_after=expire_after,
+            visibility=visibility,
+            author=User.objects.get(username=author)
+        )
+
         paste.save()
 
         return CreatePaste(paste=paste)
@@ -42,7 +52,7 @@ class UpdatePaste(graphene.Mutation):
         visibility = graphene.String()
         expire_after = graphene.String()
 
-    def mutate(self,info,id,title,text,visibility,expired_after):
+    def mutate(self, info, id, title, text, visibility, expired_after):
         paste = Paste.objects.get(id=id)
         paste.title = title
         paste.text = text
