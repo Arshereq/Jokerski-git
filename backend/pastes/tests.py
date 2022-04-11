7e55f16a13dd2677501662419b372b17fbee6700
Sebastian Murawski <oporowymakus21@gmail.com>
2022-04-11 18:03:18 +0200

    Mutation test case Create Paste

--- backend/pastes/tests.py
+++ backend/pastes/tests.py
@@ -25,3 +25,26 @@ class TestApiPasteExpectResponse(GraphQLTestCase):
 
         content = json.loads(response.content)
         self.assertResponseNoErrors(response)
+        
+def TestApiMutationCreatePaste(self):
+        response = self.query(
+        '''
+        mutation myMutation($input: MyMutationInput!){
+            createPaste(input: $input) {
+                paste{
+                    id
+                    title
+                }
+            }
+        }
+        ''',
+            op_name='createPaste',
+            input_data={
+                        "title":"Test Mutation 2 create",
+                        "author": "vat332",
+                        "text": "hdfhhdghdgdhdhgfhdgdhgf",
+                        "visibility": "True",
+                        "expired_after": "24234"
+                        }
+        )
+        self.assertResponseNoErrors(response)
