ced79f077a5d8b2c2372a5d8d524d30149a110d3
unknown <bartekniew2@gmail.com>
2022-04-12 22:59:35 +0200

    add integration with api

--- frontend/src/view/SignUp.js
+++ frontend/src/view/SignUp.js
@@ -1,32 +1,83 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ReminderPassword from './ReminderPassword';
+import { gql, useMutation } from '@apollo/client';
+import React, { useState } from 'react';
+
+const LOGIN = gql`
+mutation tokenAuth(
+    $username:String!,
+    $password:String!,
+){
+    tokenAuth(username: $username, password: $password ){
+        success,
+        errors,
+        unarchiving,
+        token,
+        refreshToken,
+        unarchiving,
+        user{
+            id,
+            username,
+        }    
+    }
+}
+`;
+
 
 function SignUp() {
+
+    const [formState, setFormState] = useState({
+        username: '',
+        password: '',
+    })
+
+
+    const [loginUser] = useMutation(LOGIN, {
+        variables: {
+            username: formState.username,
+            password: formState.password,
+        }
+    })
+
     return (
-        <div class="container">
-            <h1 class="is-size-1 mt-4 has-text-centered">Logowanie<br /><br /></h1>
-            <div class="field">
-                <label class="label">Login</label>
-                <input class="input" type="login" placeholder="Login" />
-                <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                </span>
+        <form
+            onSubmit={(e) => {
+                e.preventDefault();
+                loginUser();
+            }}>
+            <div class="container">
+                <h1 class="is-size-1 mt-4 has-text-centered">Logowanie<br /><br /></h1>
+                <div class="field">
+                    <label class="label">Login</label>
+                    <input class="input" type="login" placeholder="Login" value={formState.username}
+                        onChange={(e) => setFormState({
+                            ...formState,
+                            username: e.target.value
+                        })} />
+                    <span class="icon is-small is-left">
+                        <i class="fas fa-envelope"></i>
+                    </span>
+                </div>
+                <div class="field">
+                    <label class="label">Hasło</label>
+                    <input class="input" type="password" placeholder="Password" value={formState.password}
+                        onChange={(e) => setFormState({
+                            ...formState,
+                            password: e.target.value
+                        })} />
+                </div>
+                <div class="field">
+                    <p class="control">
+                        <button type="submit" class="button is-success mb-6">
+                            Zaloguj
+                        </button>
+                        <a class="button is-warning mb-6 ml-2" href="/ReminderPassword">
+                            Przypomnij hasło
+                        </a>
+                    </p>
+                </div>
             </div>
-            <div class="field">
-                <label class="label">Hasło</label>
-                <input class="input" type="password" placeholder="Password" />
-            </div>
-            <div class="field">
-                <p class="control">
-                    <button class="button is-success mb-6">
-                        Zaloguj
-                    </button>
-                    <a class="button is-warning mb-6 ml-2" href="/ReminderPassword">
-                        Przypomnij hasło
-                    </a>
-                </p>
-            </div>
-        </div>
+        </form>
     );
 }
 export default SignUp
